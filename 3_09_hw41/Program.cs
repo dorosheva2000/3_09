ff43aefb311d6a7a681936620243e7ffd07ce23d
user <dorosheva2000@list.ru>
2023-09-22 02:50:55 +0300

    Вторая задача

---
+++ 3_09_hw41/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int InputNumber(string massege)
+{
+    Console.WriteLine(massege);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateAray(int size)
+{
+    return new int[size];
+}
+
+void Fillarray(int[] array)
+{
+    Console.WriteLine("Введите числа");
+    for ( int i = 0; i < array.Length; i ++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+int CountPos(int[] array)
+{
+    int count = 0;
+    for ( int i = 0; i < array.Length; i ++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+
+int M = InputNumber("введите число элементов");
+int [] arr = CreateAray(M);
+Fillarray(arr);
+Console.WriteLine($"Количество чисел больше нуля:  {CountPos(arr)}");
---
+++ 3_09_hw43/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double InputNumber(string massege)
+{
+    Console.WriteLine(massege);
+    return double.Parse(Console.ReadLine()!);
+}
+
+void IntersectionPoint(double k1, double b1, double k2, double b2, double[] arr)
+{
+    arr[0] = (b2 - b1) / (k1 - k2);
+    arr[1] = k1 * arr[0] + b1;
+}
+
+double[] point = new double[2];
+double k1 = InputNumber("Введите k1");
+double b1 = InputNumber("Введите b1");
+double k2 = InputNumber("Введите k2");
+double b2 = InputNumber("Введите b2");
+
+IntersectionPoint(k1, b1, k2, b2, point);
+Console.WriteLine($"({point[0]};{point[1]})");
